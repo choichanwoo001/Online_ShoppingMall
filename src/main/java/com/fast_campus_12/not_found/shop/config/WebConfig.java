7d490186e5c5d6c7797836e99b35d56fa2176013
bwnfo3 <142577603+bwnfo3@users.noreply.github.com>
2025-06-19 18:05:44 +0900

    [Feature] 프론트 common ui 추가 (#12)
    * feat: 프론트 전반적인 폼 추가
    
    * chore: 필요 없는 파일 삭제
    
     Changes to be committed:
    	deleted:    src/main/webapp/WEB-INF/templates/hello.html
    
    * chore: Header 주석 추가
    
    layout.css에 Header 부분 명시
    - reset 주석 삭제
    
    * chore:PageController renderPage 메서드 주석 추가

--- src/main/java/com/fast_campus_12/not_found/shop/config/WebConfig.java
+++ src/main/java/com/fast_campus_12/not_found/shop/config/WebConfig.java
@@ -1,10 +1,13 @@
 package com.fast_campus_12.not_found.shop.config;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
@@ -12,7 +15,7 @@ import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.fast_campus_12.not_found.shop")
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
@@ -21,6 +24,7 @@ public class WebConfig {
         resolver.setSuffix(".html");
         resolver.setCharacterEncoding("UTF-8");
         resolver.setTemplateMode("HTML");
+        resolver.setCacheable(false);
         return resolver;
     }
 
@@ -28,14 +32,21 @@ public class WebConfig {
     public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine engine = new SpringTemplateEngine();
         engine.setTemplateResolver(templateResolver());
+        engine.addDialect(new LayoutDialect());
         return engine;
     }
 
     @Bean
-    public ViewResolver viewResolver() {
+    public ThymeleafViewResolver viewResolver() {
         ThymeleafViewResolver vr = new ThymeleafViewResolver();
         vr.setTemplateEngine(templateEngine());
         vr.setCharacterEncoding("UTF-8");
         return vr;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
     }
 }
---
+++ src/main/java/com/fast_campus_12/not_found/shop/controller/PageController.java
@@ -0,0 +1,36 @@
+package com.fast_campus_12.not_found.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String renderPage( Model model) {
+        model.addAttribute("contentPath", "index :: content");
+        return "layout/base";
+    }
+
+
+    /**
+     * 지정한 HTML 페이지를 base 레이아웃의 content 영역에 렌더링합니다.
+     *
+     * 예: localhost:8080/page/sample 접속 시,
+     *     templates/page/sample.html 파일의 content fragment가
+     *     base.html의 content 영역에 삽입되어 렌더링됩니다.
+     *
+     * @param pageName 렌더링할 페이지 이름 (HTML 파일 이름)
+     * @param model    뷰에 전달할 모델 객체
+     * @return         base 레이아웃 템플릿 이름
+     */
+    @GetMapping("/page/{pageName}")
+    public String renderPage(@PathVariable("pageName") String pageName, Model model) {
+        model.addAttribute("title", pageName + "aaaa");
+        model.addAttribute("pageName", pageName);
+        model.addAttribute("contentPath", "page/" + pageName + " :: content");
+        return "layout/base";
+    }
+
+}
