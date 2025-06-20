734a94d8ee97b4ad174e69cf98cabfd9a71dd362
Yousung Jung <jys01012@gmail.com>
2025-06-20 12:06:58 +0900

    Product, category 모델 추가 (임시) (#18)
    * feat: Product, category 모델 추가
    
    iv 변경 가능, DB 스키마 확인 x
    
    * 카테고리 항목 추가
    
    * category 위치 변경
    
    * 오타 수정
    
    ---------
    
    Co-authored-by: Wnrud01 <andyyes9716@gmail.com>

---
+++ src/main/java/com/fast_campus_12/not_found/shop/product/category/Category.java
@@ -0,0 +1,69 @@
+package com.fast_campus_12.not_found.shop.product.categgory;
+
+import java.util.List;
+
+public class Category {
+    private final int code;
+    private final String label;
+    private final String parent;
+
+    public Category(int code, String label, String parent) {
+        this.code = code;
+        this.label = label;
+        this.parent = parent;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    // 정적 데이터 목록
+    public static final List<Category> LIST = List.of(
+            // OUTER
+            new Category(0, "코트", "outer"),
+            new Category(1, "자켓", "outer"),
+            new Category(2, "수트", "outer"),
+            new Category(3, "패딩", "outer"),
+            new Category(4, "가죽", "outer"),
+            new Category(5, "바람막이", "outer"),
+            new Category(6, "야상", "outer"),
+            new Category(7, "데님", "outer"),
+
+            // TOP
+            new Category(10, "긴팔티", "top"),
+            new Category(11, "후드", "top"),
+            new Category(12, "맨투맨", "top"),
+            new Category(13, "반팔티", "top"),
+            new Category(14, "나시", "top"),
+
+            // KNIT
+            new Category(20, "긴팔니트", "knit"),
+            new Category(21, "반팔니트", "knit"),
+            new Category(22, "가디건", "knit"),
+            new Category(23, "폴라", "knit"),
+            new Category(24, "베스트", "knit"),
+
+            // SHIRT
+            new Category(30, "무지", "shirt"),
+            new Category(31, "체크", "shirt"),
+            new Category(32, "데님", "shirt"),
+            new Category(33, "스트라이프", "shirt"),
+
+            // PANTS
+            new Category(40, "데님", "pants"),
+            new Category(41, "슬랙스", "pants"),
+            new Category(42, "코튼", "pants"),
+            new Category(43, "반바지", "pants"),
+            new Category(44, "트레이닝", "pants"),
+            new Category(45, "나일론", "pants"),
+            new Category(46, "폴리", "pants")
+    );
+}
---
+++ src/main/java/com/fast_campus_12/not_found/shop/product/model/Product.java
@@ -0,0 +1,37 @@
+package com.fast_campus_12.not_found.shop.product.categgory;
+
+import com.fast_campus_12.not_found.shop.product.model.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String title;
+    private int price;
+    private int discount_price;
+    private String summary;
+    private String description;
+    private String thumbnail;
+    private int weight;
+    private int dimension;
+    private String tag;
+    private String season;
+    private double averageRating;
+    private int salesCount;
+    private int viewCount;
+    private String stockQuantity;
+    private Date availableDate;
+    private boolean enabled;
+    private Date createdAt;
+    private Date updatedAt;
+    private Date deletedAt;
+    private boolean isDeleted;
+
+    private Category category;
+}
