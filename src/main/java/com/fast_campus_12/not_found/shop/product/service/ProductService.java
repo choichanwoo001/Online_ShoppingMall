de79b45d63cbba75ccd4309d5029b0718a7f93f0
Yousung Jung <jys01012@gmail.com>
2025-06-20 16:42:54 +0900

    feat: 개발환경에서 더미데이터를 이용해 상품 리스트 동적 렌더링 (#20)
    * fix: Packge명 오류
    
     Changes to be committed:
    	modified:   src/main/java/com/fast_campus_12/not_found/shop/product/category/Category.java
    	modified:   src/main/java/com/fast_campus_12/not_found/shop/product/model/Product.java
    
    * fix: 상품 리스트에서 이미지가 오버플로우 되는 문제
    
    임시
    
    * feat: 상품 리스트 DTO
    
    * feat: 프로덕트 리스트 동적으로 렌더링
    
    Dev profile에서 가짜 데이터를 동적으로 그림
      - DB 연동 x
    Prod profile은 설정 x
    
    * chore: ProductSummary 이름 변경
    
    Dto 추가
    
    * chore: ProductService에서 현재 작성하지 않은 메서드 삭제

---
+++ src/main/java/com/fast_campus_12/not_found/shop/product/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.fast_campus_12.not_found.shop.product.service;
+
+import com.fast_campus_12.not_found.shop.product.dto.ProductSummaryDto;
+import com.fast_campus_12.not_found.shop.product.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<ProductSummaryDto> getSummaryByCategory(String category);
+}
