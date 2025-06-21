60b1a20e555eafec8ad441b80d24401b3e166fe7
can019 <jys01012@gmail.com>
2025-06-22 02:14:19 +0900

    feat: Product list database 연결
    Category 연결 x

--- src/main/java/com/fast_campus_12/not_found/shop/product/dto/ProductSummaryDto.java
+++ src/main/java/com/fast_campus_12/not_found/shop/product/dto/ProductSummaryDto.java
@@ -1,15 +1,10 @@
 package com.fast_campus_12.not_found.shop.product.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class ProductSummaryDto {
     private Long id;
     private String title;
