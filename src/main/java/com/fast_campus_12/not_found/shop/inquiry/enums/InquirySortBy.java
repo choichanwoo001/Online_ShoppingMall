c3bfd96b6461b568b7cb1d398439b992287955d0
Yousung Jung <jys01012@gmail.com>
2025-06-27 12:12:11 +0900

    공지사항 content, QnA content (#88)
    * chore: Inquiry 기능 layout
    
    * chore: Inquiry, Inquiry answer
    
    관련 admin, user, product 데이터 삽입
    
    * feat: Inquiry Database 연동
    
    * feat: Inquiry 답변
    
    * feat: 공지사항 content 노출
    
    * chore: 조회수 임시 비활성화
    
    * feat: Header에 공지사항, QNA 네비게이션 추가

---
+++ src/main/java/com/fast_campus_12/not_found/shop/inquiry/enums/InquirySortBy.java
@@ -0,0 +1,15 @@
+package com.fast_campus_12.not_found.shop.inquiry.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum InquirySortBy {
+    CREATED_AT("CREATED_AT");
+
+    private final String column;
+
+    InquirySortBy(String column) {
+        this.column = column;
+    }
+
+}
--- src/main/java/com/fast_campus_12/not_found/shop/qna/service/QnaServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.fast_campus_12.not_found.shop.qna.service;
-
-public class QnaServiceImpl {
-}
